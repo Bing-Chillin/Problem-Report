0da9908ec25bfcdc4dccbe45018245b709a6d4a3
Kovács Rozália <mozzarella.kovacs@gmail.com>
2025-07-19 21:17:50 +0200

    HTTP Put added to ReportController

--- Backend/ProblemReport.Entities/Dto/ReportCreateUpdateDto.cs
+++ Backend/ProblemReport.Entities/Dto/ReportCreateUpdateDto.cs
@@ -6,4 +6,5 @@ public class ReportCreateUpdateDto
 {
     public string SubSystem { get; set; } = string.Empty;
     public string Text { get; set; } = string.Empty;
+    public string Status { get; set; } = "Open"; // Open, Closed
 }
