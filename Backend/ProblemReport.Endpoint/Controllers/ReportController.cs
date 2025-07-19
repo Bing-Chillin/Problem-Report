3ee75b52f76428bd6ad2fc512a13074195d854fc
Kovács Rozália <mozzarella.kovacs@gmail.com>
2025-07-19 13:26:35 +0200

    Test Controller

---
+++ Backend/ProblemReport.Endpoint/Controllers/ReportController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using ProblemReport.Entities.Entity;
+
+namespace ProblemReport.Endpoint.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ReportController : ControllerBase
+{
+    [HttpGet]
+    public Report Get()
+    {
+        var report = new Report
+        {
+            SubSystem = SubSystem.PlayGround,
+            Text = "A hinta eltört a játszótéren.",
+            ImageData = null, // vagy null, ha nincs kép
+            ImageType = "jpeg",
+            Date = DateTime.Now
+        };
+        return report;
+    }
+}
