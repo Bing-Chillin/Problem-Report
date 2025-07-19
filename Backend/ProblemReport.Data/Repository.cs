dcdb3491a327a8494ba7fb41e27434bb15ac4d5f
Kovács Rozália <mozzarella.kovacs@gmail.com>
2025-07-19 21:09:51 +0200

    HTTP Put added to ReportController

--- Backend/ProblemReport.Data/Repository.cs
+++ Backend/ProblemReport.Data/Repository.cs
@@ -23,10 +23,21 @@ public class Repository<T> where T : class, IIdentity
         return ctx.Set<T>();
     }
 
-    public T? FindById(string id)
+    public T FindById(string id)
     {
-        return ctx.Set<T>().FirstOrDefault(t => t.Id == id);
+        return ctx.Set<T>().First(t => t.Id == id);
     }
+
+    public async Task UpdateAsync(T entity)
+        {
+            var old = FindById(entity.Id);
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(entity));
+            }
+            ctx.Set<T>().Update(old);
+            await ctx.SaveChangesAsync();
+        }
 
     public async Task DeleteByIdAsync(string id)
     {
